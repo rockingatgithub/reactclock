392d992e6be6282ea2dd80410f763250591ec04d
rockingatgithub <contactwithsid@gmail.com>
2020-09-27 13:40:11 +0530

    Updates

---
+++ precache-manifest.3b66606053fb288e552feec86f3251d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92315584a390561fbc3a0290bb615f55",
+    "url": "/reactclock/index.html"
+  },
+  {
+    "revision": "3cb0a61d0848913f8140",
+    "url": "/reactclock/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f272e95befe44a7f174e",
+    "url": "/reactclock/static/css/main.ae729b69.chunk.css"
+  },
+  {
+    "revision": "3cb0a61d0848913f8140",
+    "url": "/reactclock/static/js/2.636ce71b.chunk.js"
+  },
+  {
+    "revision": "659df5b420806b55437f4f9e57f60411",
+    "url": "/reactclock/static/js/2.636ce71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f272e95befe44a7f174e",
+    "url": "/reactclock/static/js/main.ed8a8847.chunk.js"
+  },
+  {
+    "revision": "50a88b0449c90d8befb7",
+    "url": "/reactclock/static/js/runtime-main.ab02a06e.js"
+  }
+]);
